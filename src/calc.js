f2b95c6de9c63b57c5a2655aefbee062a82e2ddd
PaulLAGUES <paul.lagues@epsi.fr>
2017-12-08 10:01:42 +0100

    calculatrice

--- src/calc.js
+++ src/calc.js
@@ -10,28 +10,26 @@
 
 
 function calculatrice(signe, gauche, droite) {
-    var result = null;
-    if (gauche>0 && droite<430 && droite !=0 && droite !=null){
-
-	    if (signe === "+") {
-	    	result = gauche + droite;
-	    }
-	    else if (signe === "-") {
-	    	result = gauche - droite;
-	    }
-	    else if(signe === "*") {
-	    	result = gauche * droite;
-	    }
-	    else if (signe === "/") {
-	    	result = gauche / droite;
-	    }
-	   
-
+    if (typeof gauche === 'number' && typeof droite === 'number') {
+           if (gauche>=0 && droite<430 && droite!=0) {
+            switch (signe) {
+                case "*":
+                    return gauche*droite;
+                case "+":
+                    return gauche+droite;
+                case "-":
+                    return gauche-droite;
+                case "/":
+                    return gauche/droite;
+                default:
+                    return null;
+            }
+        } else {
+               return null;
+        }
+    } else{
+        return null;
+    }
 }
 
-    return result;
-}
-
-
- 
 module.exports = calculatrice;
